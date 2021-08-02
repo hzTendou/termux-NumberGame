273a2f6597686566db46df6345fd7bf9c008972c
xTendou <80038372+xTendou@users.noreply.github.com>
2021-08-02 18:47:51 +0400

    Create game.py

---
+++ game.py
@@ -0,0 +1,35 @@
+import random
+
+number = random.randint(1, 10)
+
+player_name = input("Merhaba adın nedir?")
+
+number_of_guesses = 0
+
+print('Tamam! '+ player_name+ ' 1 ile 10 arasında bir sayı tahmin ediyorum: ')
+
+while number_of_guesses < 5:
+
+    guess = int(input())
+
+    number_of_guesses += 1
+
+    if guess < number:
+
+        print('Tahminin çok düşük')
+
+    if guess > number:
+
+        print('Tahminin çok yüksek')
+
+    if guess == number:
+
+        break
+
+if guess == number:
+
+    print('içindeki sayıyı tahmin ettin ' + str(number_of_guesses) + ' tries!')
+
+else:
+
+    print('Sayıyı tahmin edemedin, sayı ' + str(number))
